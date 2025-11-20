14598b8759e74824a51b434829122badfee1bf56
Mdlulijonas <104656650+Mdlulijonas@users.noreply.github.com>
2025-11-20 23:22:49 +0200

    Delete team.js

--- team.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const TeamSchema = new mongoose.Schema({
-  name: { type: String, required: true }, // e.g. "Engineering"
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-});
-
-module.exports = mongoose.model('Team', TeamSchema);
